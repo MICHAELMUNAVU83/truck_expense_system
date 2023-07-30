9f511cb42d300f5558505309790135663f148fb9
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-07-30 13:49:44 +0300

    Changed

--- lib/truck_expense_system_web/live/admin_panel_live/index.ex
+++ lib/truck_expense_system_web/live/admin_panel_live/index.ex
@@ -7,8 +7,51 @@ defmodule TruckExpenseSystemWeb.AdminPanelLive.Index do
       Spares.list_spares()
       |> Enum.filter(fn spare -> spare.approved == false end)
 
+    approved_spares =
+      Spares.list_spares()
+      |> Enum.filter(fn spare -> spare.approved == true end)
+
     {:ok,
      socket
+     |> assign(:approved_spares, approved_spares)
+     |> assign(:pending_spares, pending_spares)}
+  end
+
+  def handle_event("approve_spare", %{"id" => id}, socket) do
+    spare = Spares.get_spare!(id)
+    {:ok, _} = Spares.update_spare(spare, %{approved: true})
+
+    pending_spares =
+      Spares.list_spares()
+      |> Enum.filter(fn spare -> spare.approved == false end)
+
+    approved_spares =
+      Spares.list_spares()
+      |> Enum.filter(fn spare -> spare.approved == true end)
+
+    {:noreply,
+     socket
+     |> put_flash(:info, "Spare approved")
+     |> assign(:approved_spares, approved_spares)
+     |> assign(:pending_spares, pending_spares)}
+  end
+
+  def handle_event("disapprove_spare", %{"id" => id}, socket) do
+    spare = Spares.get_spare!(id)
+    {:ok, _} = Spares.update_spare(spare, %{approved: false})
+
+    pending_spares =
+      Spares.list_spares()
+      |> Enum.filter(fn spare -> spare.approved == false end)
+
+    approved_spares =
+      Spares.list_spares()
+      |> Enum.filter(fn spare -> spare.approved == true end)
+
+    {:noreply,
+     socket
+     |> put_flash(:info, "Spare approved")
+     |> assign(:approved_spares, approved_spares)
      |> assign(:pending_spares, pending_spares)}
   end
 end
