d4bc339d4f6c95bfbe3a0d3bbcb1e31cef4b8f5d
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-08-03 09:03:16 +0300

    Added bg

--- lib/truck_expense_system_web/live/page_live/index.ex
+++ lib/truck_expense_system_web/live/page_live/index.ex
@@ -1,7 +1,18 @@
 defmodule TruckExpenseSystemWeb.PageLive.Index do
   use TruckExpenseSystemWeb, :live_view
+  alias TruckExpenseSystem.Users
 
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, session, socket) do
+    if is_nil(session["user_token"]) do
+      {:ok,
+       socket
+       |> assign(:user, nil)}
+    else
+      user = Users.get_user_by_session_token(session["user_token"])
+
+      {:ok,
+       socket
+       |> assign(:user, user)}
+    end
   end
 end
